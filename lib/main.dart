a39bb4ade1a877a43a7a594cdc89705794ec7730
ethan <ethanh8093@gmail.com>
2021-10-23 16:24:56 -0700

    Added a possible Method
    - Please check if it works :(

--- lib/main.dart
+++ lib/main.dart
@@ -100,7 +100,10 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .headline4,
             ),
           ],
         ),
@@ -111,5 +114,87 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  class Login extends State<LoginState> {
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+  backgroundColor: Colors.white,
+  appBar: AppBar(
+  title: Text("Login Page"),
+  ),
+  body: SingleChildScrollView(
+  child: Column(
+  children: <Widget>[
+  Padding(
+  padding: const EdgeInsets.only(top: 40.0),
+  child: Center(
+  child: Container(
+  width: 200,
+  height: 150,
+  decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),
+  child: Image.asset('https://www.seekpng.com/png/small/811-8117274_pizza-slice-clipart-black-and-white-pizza-slice.png')),
+  ),
+  ),
+  Padding(
+  //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+  padding: EdgeInsets.symmetric(horizontal: 15),
+  child: TextField(
+  decoration: InputDecoration(
+  border: OutlineInputBorder(),
+  labelText: 'Email',
+  hintText: 'Enter valid email id such as example@tastii.com'),
+  ),
+  ),
+  Padding(
+  padding: const EdgeInsets.only(
+  left: 15.0, right: 15.0, top: 15, bottom: 0),
+  padding: EdgeInsets.symmetric(horizontal: 15),
+  child: TextField(
+
+  obscureText: true,
+  decoration: InputDecoration(
+  border: OutlineInputBorder(),
+  labelText: 'Password',
+  hintText: 'Enter secure password'),
+  ),
+  ),
+  FlatButton(
+  onPressed: (){
+  //TODO FORGOT PASSWORD SCREEN GOES HERE
+  },
+  child: Text(
+  'Forgot Password',
+  style: TextStyle(color: Colors.blue, fontSize: 15),
+  ),
+  ),
+  Container(
+  height: 50,
+  width: 250,
+  decoration: BoxDecoration(
+  color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+  child: FlatButton(
+  onPressed: () {
+  Navigator.push(
+  context, MaterialPageRoute(builder: (_) => HomePage()));
+  },
+  child: Text(
+  'Login',
+  style: TextStyle(color: Colors.blue, fontSize: 20),
+  ),
+  ),
+  ),
+  SizedBox(
+  height: 150,
+  ),
+  Text('New User? Create Account')
+  ],
+  ),
+  ),
+  );
+  }
   }
 }
