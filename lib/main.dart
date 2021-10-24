8b6ab424ea221ba17f3acf12dbeb2c2736b00cbb
ethan <ethanh8093@gmail.com>
2021-10-24 00:02:31 -0700

    Minor Housekeeping

--- lib/main.dart
+++ lib/main.dart
@@ -1,250 +1,181 @@
+import 'package:tastii/auth/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized;
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: SignUp(),
-    );
-  }
+  _AppState createState() => _AppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          // return SomethingWentWrong();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
             ),
-            Text(
-              '$_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            home: SignUp(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Text("Loading");
+      },
     );
   }
 
-  class Login extends State<LoginState> {
-  @override
-  Widget build(BuildContext context) {
-  return Scaffold(
-  backgroundColor: Colors.white,
-  appBar: AppBar(
-  title: Text("Login Page"),
-  ),
-  body: SingleChildScrollView(
-  child: Column(
-  children: <Widget>[
-  Padding(
-  padding: const EdgeInsets.only(top: 40.0),
-  child: Center(
-  child: Container(
-  width: 200,
-  height: 150,
-  decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(50.0)),
-  child: Image.asset('https://www.seekpng.com/png/small/811-8117274_pizza-slice-clipart-black-and-white-pizza-slice.png')),
-  ),
-  ),
-  Padding(
-  //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-  padding: EdgeInsets.symmetric(horizontal: 15),
-  child: TextField(
-  decoration: InputDecoration(
-  border: OutlineInputBorder(),
-  labelText: 'Email',
-  hintText: 'Enter valid email id such as example@tastii.com'),
-  ),
-  ),
-  Padding(
-  padding: const EdgeInsets.only(
-  left: 15.0, right: 15.0, top: 15, bottom: 0),
-  padding: EdgeInsets.symmetric(horizontal: 15),
-  child: TextField(
+// class Login extends State<LoginState> {
+// @override
+// Widget build(BuildContext context) {
+// return Scaffold(
+// backgroundColor: Colors.white,
+// appBar: AppBar(
+// title: Text("Login Page"),
+// ),
+// body: SingleChildScrollView(
+// child: Column(
+// children: <Widget>[
+// Padding(
+// padding: const EdgeInsets.only(top: 40.0),
+// child: Center(
+// child: Container(
+// width: 200,
+// height: 150,
+// decoration: BoxDecoration(
+//                       color: Colors.red,
+//                       borderRadius: BorderRadius.circular(50.0)),
+// child: Image.asset('https://www.seekpng.com/png/small/811-8117274_pizza-slice-clipart-black-and-white-pizza-slice.png')),
+// ),
+// ),
+// Padding(
+// //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+// padding: EdgeInsets.symmetric(horizontal: 15),
+// child: TextField(
+// decoration: InputDecoration(
+// border: OutlineInputBorder(),
+// labelText: 'Email',
+// hintText: 'Enter valid email id such as example@tastii.com'),
+// ),
+// ),
+// Padding(
+// padding: const EdgeInsets.only(
+// left: 15.0, right: 15.0, top: 15, bottom: 0),
+// padding: EdgeInsets.symmetric(horizontal: 15),
+// child: TextField(
 
-  obscureText: true,
-  decoration: InputDecoration(
-  border: OutlineInputBorder(),
-  labelText: 'Password',
-  hintText: 'Enter secure password'),
-  ),
-  ),
-  FlatButton(
-  onPressed: (){
-  //TODO FORGOT PASSWORD SCREEN GOES HERE
-  },
-  child: Text(
-  'Forgot Password',
-  style: TextStyle(color: Colors.blue, fontSize: 15),
-  ),
-  ),
-  Container(
-  height: 50,
-  width: 250,
-  decoration: BoxDecoration(
-  color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-  child: FlatButton(
-  onPressed: () {
-  Navigator.push(
-  context, MaterialPageRoute(builder: (_) => HomePage()));
-  },
-  child: Text(
-  'Login',
-  style: TextStyle(color: Colors.blue, fontSize: 20),
-  ),
-  ),
-  ),
-  SizedBox(
-  height: 150,
-  ),
-  Text('New User? Create Account')
-  ],
-  ),
-  ),
-  );
-  }
-  }
+// obscureText: true,
+// decoration: InputDecoration(
+// border: OutlineInputBorder(),
+// labelText: 'Password',
+// hintText: 'Enter secure password'),
+// ),
+// ),
+// FlatButton(
+// onPressed: (){
+// //TODO FORGOT PASSWORD SCREEN GOES HERE
+// },
+// child: Text(
+// 'Forgot Password',
+// style: TextStyle(color: Colors.blue, fontSize: 15),
+// ),
+// ),
+// Container(
+// height: 50,
+// width: 250,
+// decoration: BoxDecoration(
+// color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+// child: FlatButton(
+// onPressed: () {
+// Navigator.push(
+// context, MaterialPageRoute(builder: (_) => HomePage()));
+// },
+// child: Text(
+// 'Login',
+// style: TextStyle(color: Colors.blue, fontSize: 20),
+// ),
+// ),
+// ),
+// SizedBox(
+// height: 150,
+// ),
+// Text('New User? Create Account')
+// ],
+// ),
+// ),
+// );
+// }
+// }
 
-  class SignUp extends StatefulWidget {
-    SignUp({Key key}) : super(key: key);
+// class SignUp extends StatefulWidget {
+//   SignUp({Key key}) : super(key: key);
 
-    @override
-    _SignUpState createState() => _SignUpState();
-  }
+//   @override
+//   _SignUpState createState() => _SignUpState();
+// }
 
-  class _SignUpState extends State<SignUp> {
-    
-    String email = '';
-    String password = '';
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar:AppBar(
-          backgroundColor: Colors.blue,
-          elevation: 8,
-          title: Text("Sign Up")
-        )
-        body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-          child:new Form(
-            child: Column(
-              children: [
-                TextFormField(
-                  onChanged: (val) => setState((){
-                    email = val;
-                  }),
-                ),
-                TextFormField(
-                  onChanged: (val) => setState((){
-                    email = val;
-                  }),
-                ),
-                RaisedButton(child: Text('SignUp'), onPressed: null)
-              ],
-            )
-          )),
-        ));
-      )
-    }
-  }
+// class _SignUpState extends State<SignUp> {
 
-  RaisedButton(
-    child:Text('Signup'),
-    onPressed: () async => {
+//   String email = '';
+//   String password = '';
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar:AppBar(
+//         backgroundColor: Colors.blue,
+//         elevation: 8,
+//         title: Text("Sign Up")
+//       )
+//       body: Container(
+//         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+//         child:new Form(
+//           child: Column(
+//             children: [
+//               TextFormField(
+//                 onChanged: (val) => setState((){
+//                   email = val;
+//                 }),
+//               ),
+//               TextFormField(
+//                 onChanged: (val) => setState((){
+//                   email = val;
+//                 }),
+//               ),
+//               RaisedButton(child: Text('SignUp'), onPressed: null)
+//             ],
+//           )
+//         )),
+//       ));
+//     )
+//   }
+// }
 
-    }
-  )
+// RaisedButton(
+//   child:Text('Signup'),
+//   onPressed: () async => {
+
+//   }
+// )
 }
